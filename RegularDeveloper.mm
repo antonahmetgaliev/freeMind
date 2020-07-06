cc4f9788c88f6b5ba19daf91b9ace9701e3eadab
antonahmetgaliev <antonahmetgaliev4@gmail.com>
2020-07-06 10:32:17 +0300

    Updated Regular developer map

--- RegularDeveloper.mm
+++ RegularDeveloper.mm
@@ -1,7 +1,29 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1592806997938" ID="ID_253434183" MODIFIED="1592807063600" TEXT="Regular developer">
-<node CREATED="1592807067633" ID="ID_1638629468" MODIFIED="1592807074929" POSITION="right" TEXT="Patterns">
+<node CREATED="1592806997938" ID="ID_253434183" MODIFIED="1594018019997" TEXT="Regular developer">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://github.com/antonahmetgaliev/freeMind">https://github.com/antonahmetgaliev/freeMind</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1592807067633" ID="ID_1638629468" MODIFIED="1594018105479" POSITION="right" TEXT="Patterns">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://github.com/antonahmetgaliev/Education/tree/master/Patterns">https://github.com/antonahmetgaliev/Education/tree/master/Patterns</a>
+    </p>
+  </body>
+</html>
+</richcontent>
 <node CREATED="1594015884910" ID="ID_1864505400" MODIFIED="1594015913975" TEXT="Principles">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -18,10 +40,9 @@
       - &#1055;&#1088;&#1086;&#1075;&#1088;&#1072;&#1084;&#1084;&#1080;&#1088;&#1091;&#1081;&#1090;&#1077; &#1085;&#1072; &#1091;&#1088;&#1086;&#1074;&#1085;&#1077; &#1080;&#1085;&#1090;&#1077;&#1088;&#1092;&#1077;&#1081;&#1089;&#1086;&#1074;, &#1072; &#1085;&#1077; &#1088;&#1077;&#1072;&#1083;&#1080;&#1079;&#1072;&#1094;&#1080;&#1080;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
-<node CREATED="1592807204507" ID="ID_1105369281" MODIFIED="1594015877961" TEXT="Strategy">
+<node CREATED="1592807204507" ID="ID_1105369281" MODIFIED="1594016990274" TEXT="Strategy">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -46,14 +67,95 @@
       - &#1055;&#1088;&#1086;&#1075;&#1088;&#1072;&#1084;&#1084;&#1080;&#1088;&#1091;&#1081;&#1090;&#1077; &#1085;&#1072; &#1091;&#1088;&#1086;&#1074;&#1085;&#1077; &#1080;&#1085;&#1090;&#1077;&#1088;&#1092;&#1077;&#1081;&#1089;&#1086;&#1074;, &#1072; &#1085;&#1077; &#1088;&#1077;&#1072;&#1083;&#1080;&#1079;&#1072;&#1094;&#1080;&#1080;
     </p>
   </body>
+</html></richcontent>
+</node>
+<node CREATED="1594015778146" ID="ID_1482603729" MODIFIED="1594017110710" TEXT="Observer">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1054;&#1087;&#1088;&#1077;&#1076;&#1077;&#1083;&#1103;&#1077;&#1090; &#1086;&#1090;&#1085;&#1086;&#1096;&#1077;&#1085;&#1080;&#1077; &quot;&#1086;&#1076;&#1080;&#1085;-&#1082;&#1086;-&#1084;&#1085;&#1086;&#1075;&#1080;&#1084;&quot; &#1084;&#1077;&#1078;&#1076;&#1091; &#1086;&#1073;&#1098;&#1077;&#1082;&#1090;&#1072;&#1084;&#1080; &#1090;&#1072;&#1082;&#1080;&#1084; &#1086;&#1073;&#1088;&#1072;&#1079;&#1086;&#1084;, &#1095;&#1090;&#1086; &#1087;&#1088;&#1080; &#1080;&#1079;&#1084;&#1077;&#1085;&#1077;&#1085;&#1080;&#1080; &#1089;&#1086;&#1089;&#1090;&#1086;&#1103;&#1085;&#1080;&#1103; &#1086;&#1076;&#1085;&#1086;&#1075;&#1086; &#1086;&#1073;&#1098;&#1077;&#1082;&#1090;&#1072; &#1087;&#1088;&#1086;&#1080;&#1089;&#1093;&#1086;&#1076;&#1080;&#1090; &#1072;&#1074;&#1090;&#1086;&#1084;&#1072;&#1090;&#1080;&#1095;&#1077;&#1089;&#1082;&#1086;&#1077; &#1086;&#1087;&#1074;&#1077;&#1097;&#1077;&#1085;&#1080;&#1077; &#1080; &#1086;&#1073;&#1085;&#1086;&#1074;&#1083;&#1077;&#1085;&#1080;&#1077; &#1074;&#1089;&#1077;&#1093; &#1079;&#1072;&#1074;&#1080;&#1089;&#1080;&#1084;&#1099;&#1093; &#1086;&#1073;&#1098;&#1077;&#1082;&#1090;&#1086;&#1074;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1594017110699" ID="ID_1309227633" MODIFIED="1594017118878" TEXT="Decorator"/>
+<node CREATED="1594017120103" ID="ID_1843032926" MODIFIED="1594017149788" TEXT="Factory method"/>
+<node CREATED="1594017150637" ID="ID_1115333170" MODIFIED="1594017156045" TEXT="Abstract factory"/>
+<node CREATED="1594017163338" ID="ID_1060987028" MODIFIED="1594017171969" TEXT="Singleton"/>
+<node CREATED="1594017173499" ID="ID_1215489661" MODIFIED="1594017179710" TEXT="Command"/>
+<node CREATED="1594017180189" ID="ID_1356485700" MODIFIED="1594017188375" TEXT="Adapter"/>
+<node CREATED="1594017188912" ID="ID_283432141" MODIFIED="1594017217881" TEXT="Facade"/>
+<node CREATED="1594017218338" ID="ID_1746204984" MODIFIED="1594017227960" TEXT="Template method"/>
+<node CREATED="1594017246630" ID="ID_1351317936" MODIFIED="1594017250392" TEXT="Iterator"/>
+<node CREATED="1594017280697" ID="ID_1584729788" MODIFIED="1594017281974" TEXT="Composer"/>
+<node CREATED="1594017301532" ID="ID_663055103" MODIFIED="1594017306079" TEXT="Condition"/>
+<node CREATED="1594018299711" ID="ID_1420420739" MODIFIED="1594018307096" TEXT="MVC"/>
+</node>
+<node CREATED="1594018977933" ID="ID_1384774683" MODIFIED="1594018981366" POSITION="right" TEXT="Antipatterns"/>
+<node CREATED="1594018560431" ID="ID_1181668139" MODIFIED="1594018565883" POSITION="right" TEXT=".Net">
+<node CREATED="1594018573029" ID="ID_758920361" MODIFIED="1594018580943" TEXT="OOP">
+<node CREATED="1592807078021" ID="ID_498531475" MODIFIED="1594018601077" TEXT="Richter">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://github.com/antonahmetgaliev/Education/tree/master/.NET">https://github.com/antonahmetgaliev/Education/tree/master/.NET</a>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="yes"/>
+</node>
+<node CREATED="1594018637077" ID="ID_1478780153" MODIFIED="1594018843448" TEXT="SOLID">
+<node CREATED="1594018660600" ID="ID_936481969" MODIFIED="1594018845057" TEXT="Single responsibility principle">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1594018676651" ID="ID_772295884" MODIFIED="1594018847439" TEXT="Open Closed principle">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1594018697758" ID="ID_1428495285" MODIFIED="1594018808887" TEXT="Liscow substitution principle"/>
+<node CREATED="1594018714099" ID="ID_1862071362" MODIFIED="1594018852659" TEXT="Interface segregation principle">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1594018729254" ID="ID_1026886758" MODIFIED="1594018854578" TEXT="Dependencies inversion principle">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1594019019941" ID="ID_363474624" MODIFIED="1594019027121" TEXT="LINQ"/>
+</node>
+<node CREATED="1592807093813" ID="ID_799784699" MODIFIED="1592807107707" POSITION="right" TEXT=".Net core">
+<node CREATED="1594017989782" ID="ID_1237611225" MODIFIED="1594018214655" TEXT="TelegramBots">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <a href="https://github.com/mihail1orlov/TelegramBotsService">https://github.com/mihail1orlov/TelegramBotsService</a>
+    </p>
+  </body>
 </html>
 </richcontent>
 <icon BUILTIN="idea"/>
 </node>
-<node CREATED="1594015778146" ID="ID_1482603729" MODIFIED="1594015790797" TEXT="Observer"/>
+<node CREATED="1594018514980" ID="ID_589776175" MODIFIED="1594018526986" TEXT="ASP .Net core"/>
+<node CREATED="1594018527544" ID="ID_1352571429" MODIFIED="1594018533542" TEXT="Angular"/>
 </node>
-<node CREATED="1592807078021" ID="ID_498531475" MODIFIED="1592807092706" POSITION="right" TEXT="Richter"/>
-<node CREATED="1592807093813" ID="ID_799784699" MODIFIED="1592807107707" POSITION="right" TEXT=".Net core"/>
-<node CREATED="1592807108582" ID="ID_1355118649" MODIFIED="1592807108582" POSITION="right" TEXT=""/>
+<node CREATED="1594018240261" ID="ID_1712212675" MODIFIED="1594018243059" POSITION="right" TEXT="Docker"/>
+<node CREATED="1594018394155" ID="ID_1040619513" MODIFIED="1594018406984" POSITION="right" TEXT="SQL"/>
+<node CREATED="1594018937234" ID="ID_728544501" MODIFIED="1594018946133" POSITION="right" TEXT="Unit tests"/>
+<node CREATED="1592807108582" ID="ID_1355118649" MODIFIED="1594018222337" POSITION="left" TEXT="English">
+<icon BUILTIN="yes"/>
+<node CREATED="1594018162013" ID="ID_1082047163" MODIFIED="1594018211714" TEXT="B1 Intermediate">
+<icon BUILTIN="idea"/>
+</node>
+</node>
 </node>
 </map>
